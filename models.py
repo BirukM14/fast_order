b19ebd03ac4a1dd7e27be7b201030b3cffe88ef1
BirukM14 <gbiruka14@gmail.com>
2025-03-18 14:40:43 +0300

    Fix ForeignKey import typo and improve formatting in models.py

--- models.py
+++ models.py
@@ -1,43 +1,42 @@
 from database import Base
-from sqlalchemy import Column,Integer,Boolean,Text,String,ForignKey
+from sqlalchemy import Column, Integer, Boolean, Text, String, ForeignKey
 from sqlalchemy_utils import ChoiceType
-
 from sqlalchemy.orm import relationship
 
 class User(Base):
-    __tablename__='user'
-    id=Column(Integer,primary_key=True)
-    username=Column(String(25),unique=True)
-    email=Column(String(80),unique=True)
-    password=Column(Text,nullable=True)
-    is_staff=Column(Boolean,default=False)
-    is_active=Column(Boolean,default=False)
-    orders=relationship('Order', back_populates=='user')
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(25), unique=True)
+    email = Column(String(80), unique=True)
+    password = Column(Text, nullable=True)
+    is_staff = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=False)
+    orders = relationship('Order', back_populates='user')
 
     def __repr__(self):
-        return f"<User {self.username}"
+        return f"<User {self.username}>"
 
-class Order (Base):
+class Order(Base):
+    ORDER_STATUSES = (
+        ('PENDING', 'pending'),
+        ('IN-TRANSIT', 'in-transit'),
+        ('DELIVERED', 'delivered')
+    )
 
-    ORDER_STATUSES=(
-        ('PENDING','pending'),
-        ('IN-TRANSIT','in-transit'),
-        ('DELEVERED','delivered')
+    PIZZA_SIZES = (
+        ('SMALL', 'small'),
+        ('MEDIUM', 'medium'),
+        ('LARGE', 'large'),
+        ('EXTRA-LARGE', 'extra-large')
     )
-    PIZZA_SIZES=(
-        ('SMALL','small'),
-        ('MEDIUM','medium'),
-        ('LARGE','large'),
-        ('EXTRA-LARGE','extra-large')
-    )
-    __tablename__="orders"
-    id=Column(Integer,primary_key=True)
-    quantity=Column(Integer,nullable=True)
-    order_statuses=Column(ChoiceType(Choices=ORDER_STATUSES,default='pending'))
-    pizza_size=Column(ChoiceType(Choices=PIZZA_SIZES, default='SMALL'))
-    user_id=Column(Integer,ForignKey('user.id'))
-    users=relationship('User', back_populates=='orders')
+
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer, nullable=True)
+    order_status = Column(ChoiceType(choices=ORDER_STATUSES), default='pending')
+    pizza_size = Column(ChoiceType(choices=PIZZA_SIZES), default='SMALL')
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='orders')
 
     def __repr__(self):
-        return f"<Order  {self.id}>"
-
+        return f"<Order {self.id}>"
