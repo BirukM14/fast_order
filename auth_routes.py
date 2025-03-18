f6194d756e3f013fc6affd0653eb7ec74faffa94
BirukM14 <gbiruka14@gmail.com>
2025-03-18 15:13:37 +0300

    Fix signup route to check for existing user and handle errors

--- auth_routes.py
+++ auth_routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from database import Session, engine
+from models import user
+from fastapi.exceptions import HTTPException
 
 auth_router = APIRouter(
 
@@ -17,4 +19,10 @@ async def hello():
 
 
 @auth_router.post('/signup')
-async def signup(user:SignUpModlel)
+async def signup(user:SignUpModlel):
+    db_email-session.query(User).filter(User.email=user.email).first()
+
+    if db-email is not None:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+        detail="user with the username already exists"
+        )
