d34b1d7b21a1d8485f5d7862f6301b50907109d2
BirukM14 <gbiruka14@gmail.com>
2025-03-18 14:10:27 +0300

    Add models module for data structures

--- database.py
+++ database.py
@@ -1,1 +1,11 @@
-f
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base,sessionmaker
+
+
+engine = create_engine('postgresql://postgres:Bb@0939852436@localhost/postgres'
+    echo=True
+)
+
+Base=declarative_base()
+
+Session=sessionmaker()
