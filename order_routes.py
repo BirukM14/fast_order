23f8ec67613df5d37b59abcabd61263f36c5ce55
BirukM14 <gbiruka14@gmail.com>
2025-03-21 15:15:16 +0300

    Add JWT authorization to order routes and handle unauthorized access

--- order_routes.py
+++ order_routes.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter,Depends
 from fastapi _jwt_auth import AuthJWT
 from models import User,Order
 from schemas import orderModel
+from fastapi.exceptions import HTTPException
+from data
 
 
 order_router = APIRouter()
@@ -9,5 +11,38 @@ order_router = APIRouter()
 
 @order_router.get('/')
 async def hello(Authorize:AuthJWT=Depends()):
-    
+
+    try:
+        Authorize.jwt_required()
+
+
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="invalid token"
+        )
+
+    return {"message":"hellow worman"}
+
+
+@order_router.post('/')
+
+async def place _an_order(order:orderModel,Authorize:AuthJWT=depends())
+
+    try:
+        Authorize.jwt_required()
+
+
+    except Exception as e:
+        raise HTTPException (
+        status_code=status.HTTP_401_UNAUTHORIZED.
+        detail="invalid token"
+    )
+
+    current_user=Authorize.get_jwt_subject()
+
+
+
+
 
